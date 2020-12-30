e0ced74abd2f6771391e607c653f4233b4bf713d
Dilan <dio4@pitt.edu>
2020-12-30 18:11:32 -0500

    postinstall.js checks for previous line

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,7 +6,9 @@ try {
         
         var newIndex = data.lastIndexOf("end");
         var newLine = "  # easybase-react dependency\n  pod 'RNCAsyncStorage', :path => '../node_modules/@react-native-community/async-storage'\n";
-        fs.writeFileSync("../../ios/Podfile", data.slice(0, newIndex) + newLine + data.slice(newIndex)); 
+        if (!data.includes(newLine)) {
+            fs.writeFileSync("../../ios/Podfile", data.slice(0, newIndex) + newLine + data.slice(newIndex)); 
+        }
     } else {
         console.log("Skipping optional pod install")
     }
