d657fbaca15bafe20583f1ce44ccbffb2975990c
Dilan <dio4@pitt.edu>
2020-12-31 12:38:25 -0500

    Clearing cache in postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,6 +2,8 @@ var fs = require("fs");
 var execSync = require("child_process").execSync;
 var commandExistsSync = require("./command-exists").sync;
 
+var isUsingWindows = process.platform === "win32";
+
 try {
     if (fs.existsSync("../../ios/Podfile")) {
         var data = fs.readFileSync('../../ios/Podfile', 'utf-8');
@@ -10,7 +12,18 @@ try {
         var newLine = "  # easybase-react dependency\n  pod 'RNCAsyncStorage', :path => '../node_modules/@react-native-community/async-storage'\n";
         if (!data.includes(newLine)) {
             fs.writeFileSync("../../ios/Podfile", data.slice(0, newIndex) + newLine + data.slice(newIndex));
-            if (commandExistsSync('xcodebuild')) {
+            
+            try {
+                if (isUsingWindows) {
+                    execSync("del %appdata%\\Temp\\react-native-*");
+                } else {
+                    execSync("rm -rf $TMPDIR/metro-*");
+                }
+            } catch (error) {
+                console.log("tmp cache not cleared");
+            }
+
+            if (!isUsingWindows && commandExistsSync('xcodebuild')) {
                 try {
                     // xcodebuild clean forces RN to rebuild iOS app on the next start
                     execSync("cd .. && cd .. && cd ios && xcodebuild clean", {
