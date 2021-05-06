68d74300f5c99501500f199b6c0c71146821cb6b
Dilan <dio4@pitt.edu>
2021-05-06 15:48:42 -0400

    Cancelling callbacks on useReturn unmount

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -15,28 +15,35 @@ import {
 } from "react-router-dom";
 import ProjectUser from "./ProjectUser";
 import UseReturnExample from './UseReturnExample';
+import UseReturnStressTest from './UseReturnStressTest';
 
-const IntegrationExample = () => (
-  <EasybaseProvider ebconfig={ebconfig} options={{ logging: false }}>
-    <h2>Visual Query</h2>
-    <QueryExample />
-    <hr className="m-4" />
-    <h2>Function</h2>
-    <FunctionExample />
-    <hr className="m-4" />
-    <h2>Db</h2>
-    <DbExample />
-    <hr className="m-4" />
-    <h2>Use Return</h2>
-    <UseReturnExample />
-  </EasybaseProvider>
-)
+const IntegrationExample = () => {
+  React.useEffect(() => console.log("MOUNTING INTEGRATION EXAMPLE"), []);
+  return (
+    <EasybaseProvider ebconfig={ebconfig} options={{ logging: false }}>
+      <h2>Visual Query</h2>
+      <QueryExample />
+      <hr className="m-4" />
+      <h2>Function</h2>
+      <FunctionExample />
+      <hr className="m-4" />
+      <h2>Db</h2>
+      <DbExample />
+      <hr className="m-4" />
+      <h2>Use Return</h2>
+      <UseReturnExample />
+    </EasybaseProvider>
+  )
+}
 
-const ProjectExample = () => (
-  <EasybaseProvider ebconfig={ebconfig2} options={{ logging: true }}>
-    <ProjectUser />
-  </EasybaseProvider>
-)
+const ProjectExample = () => {
+  React.useEffect(() => console.log("MOUNTING PROJECT EXAMPLE"), []);
+  return (
+    <EasybaseProvider ebconfig={ebconfig2} options={{ logging: true }}>
+      <ProjectUser />
+    </EasybaseProvider>
+  )
+}
 
 const App = () => {
   return (
@@ -48,6 +55,9 @@ const App = () => {
         <li>
           <Link to="/project">Project Example</Link>
         </li>
+        <li>
+          <Link to="/usereturnstresstest">useReturn Stress Test</Link>
+        </li>
       </ul>
 
       <Switch>
@@ -56,6 +66,11 @@ const App = () => {
         </Route>
         <Route path="/project" exact >
           <ProjectExample />
+        </Route>
+        <Route path="/usereturnstresstest" exact >
+          <EasybaseProvider ebconfig={ebconfig} options={{ logging: true }}>
+            <UseReturnStressTest />
+          </EasybaseProvider>
         </Route>
       </Switch>
     </HashRouter>
