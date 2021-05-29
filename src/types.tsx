f1f00035b9669b4618f11a439aa0c4139965decf
Dilan <dio4@pitt.edu>
2021-05-28 22:27:38 -0400

    fixed import of external types in types.d.ts

--- src/types.tsx
+++ src/types.tsx
@@ -4,7 +4,7 @@ import { NewExpression } from "EasyQB/types/expression";
 import {
     EXECUTE_COUNT,
     DB_STATUS
-} from "easybasejs/dist/EasybaseProvider/types"
+} from "easybasejs/src/EasybaseProvider/types"
 
 import type {
     Ebconfig,
@@ -16,7 +16,7 @@ import type {
     AddRecordOptions,
     DeleteRecordOptions,
     EmailTemplate
-} from "easybasejs/dist/EasybaseProvider/types"
+} from "easybasejs/src/EasybaseProvider/types"
 
 export type {
     FileFromURI,
@@ -30,13 +30,13 @@ export type {
     AddRecordOptions,
     DeleteRecordOptions,
     EmailTemplate
-} from "easybasejs/dist/EasybaseProvider/types"
+} from "easybasejs/src/EasybaseProvider/types"
 
 export {
     POST_TYPES,
     EXECUTE_COUNT,
     DB_STATUS
-} from "easybasejs/dist/EasybaseProvider/types"
+} from "easybasejs/src/EasybaseProvider/types"
 
 export interface EasybaseProviderPropsOptions {
     /** Custom authentication string. Can be set in integration menu. If it is set, it is required to access integration. This acts as an extra layer of security and extensibility. */
