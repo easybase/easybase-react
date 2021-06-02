79dc44f7598f229ad2be1f94c96265911e17bf0e
Dilan <dio4@pitt.edu>
2021-06-02 16:03:47 -0400

    Implement React Native example in code

---
+++ native-example/App.js
@@ -0,0 +1,87 @@
+/* eslint-disable operator-linebreak */
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useEasybase, EasybaseProvider } from 'easybase-react';
+import ebconfig from './ebconfig';
+
+function Account() {
+  const [userVal, setUserVal] = useState("");
+  const [passVal, setPassVal] = useState("");
+
+  const { signIn, signUp } = useEasybase();
+
+  const clearInputs = () => {
+    setUserVal("");
+    setPassVal("");
+  }
+
+  const handleSignInPress = async () => {
+    await signIn(userVal, passVal);
+    clearInputs();
+  }
+
+  const handleSignUpPress = async () => {
+    const res = await signUp(userVal, passVal, {
+      created_at: new Date().toString
+    });
+    if (res.success) {
+      await signIn(userVal, passVal);
+    }
+    clearInputs();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to React-flix!</Text>
+      <TextInput value={userVal} onChangeText={e => setUserVal(e)} style={styles.accountInput} placeholder="Username" />
+      <TextInput value={passVal} onChangeText={e => setPassVal(e)} style={styles.accountInput} placeholder="Password"/>
+      <View style={{ display: "flex", flexDirection: "row", marginTop: 30 }}>
+        <Button title="Sign In" onPress={handleSignInPress} />
+        <Button title="Sign Up" onPress={handleSignUpPress} />
+      </View>
+    </View>
+  )
+}
+
+function Router() {
+  const { isUserSignedIn } = useEasybase();
+
+  return (
+    isUserSignedIn() ?
+      <Text>Congrats! You're signed in.</Text>
+      :
+      <Account />
+  )
+}
+
+export default function App() {
+  return (
+    <EasybaseProvider ebconfig={ebconfig}>
+      <Router />
+    </EasybaseProvider>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  accountInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: "75%",
+    margin: 10,
+    fontSize: 22,
+    textAlign: "center"
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "500",
+    fontStyle: "italic",
+    marginBottom: 30
+  }
+});
