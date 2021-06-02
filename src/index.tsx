13b00e5259fb1948397306c0872134f37113dc01
Dilan <dio4@pitt.edu>
2021-06-02 14:24:08 -0400

    Ready for NativeAuth workflow

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 export { default as EasybaseProvider } from './EasybaseProvider';
 export { default as useEasybase } from './useEasybase';
 export { default as Auth } from './ui/Auth';
+// export { default as NativeAuth } from './ui/NativeAuth';
 export { callFunction } from './callFunction';
