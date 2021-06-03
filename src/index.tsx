bc356375b205adbde4872c3d3b3d2ba2452c73e2
Dilan <dio4@pitt.edu>
2021-06-03 10:15:52 -0400

    Fixed old export of native auth from src/index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 export { default as EasybaseProvider } from './EasybaseProvider';
 export { default as useEasybase } from './useEasybase';
 export { default as Auth } from './ui/Auth';
-export { default as NativeAuth } from './ui/NativeAuth';
 export { callFunction } from './callFunction';
